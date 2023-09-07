697fd32e09b8dc6fe7bfb84a0a99948038dc1e55
Shreyas Prabhu <shreyasprabhu26@gmail.com>
2023-09-07 11:39:50 +0530

    Manipulate Dom Text

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 "use strict"
 
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const quoteAuthor = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 function newQuote() {
+    //Get the Quote Data
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    const author = quote?.author ?? "Unknown";
+
+    //Check if Quote Lenght is large and change the style
+    (quote.text.length > 50) ? quoteText.classList.add("long-quote") : quoteText.classList.remove("long-quote")
+
+    //Set Dom Fields
+    quoteAuthor.textContent = author;
+    quoteText.textContent = quote.text;
 }
 
 let apiQuotes = [];
